556408c93949315ed03341739882e6c838a7df5b
loofoomj@naver.com <ec2-user@ip-172-31-26-24.ap-northeast-1.compute.internal>
2016-11-14 11:37:11 +0000

    회원가입 정보 저장 및 화면 전환 기능 추가하기

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,23 @@
 class UserController < ApplicationController
   def signup
   end
+
+  def signup_complete
+  	u = User.new
+  	u.username = params[:username]
+  	if params[:password] == params[:retype_password]
+  		u.password = params[:password]
+  		if u.save
+  			flash[:alert] = "회원가입 완료"
+  			redirect_to "wall/posts"
+  		else
+  			flash[:alert] = u.errors.values.flatten.join(' ')
+  			redirect_to :back
+  		end
+  	else
+  		flash[:alert] = "패스워드가 일치하지 않습니다."
+  		redirect_to :back
+  	end
+  	
+  end
 end
